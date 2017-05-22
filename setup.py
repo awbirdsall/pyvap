b8a48c216c6af611a296b322f9cf253d11fd8bb0
awbirdsall <abirdsall@gmail.com>
2017-05-22 14:08:54 -0400

    reorganize into package
    - put code in /src/pyvap/
    - add version.py and __init__.py in src
    - add setup.py to root
    - update .gitignore

---
+++ setup.py
@@ -0,0 +1,47 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+exec(open('src/pyvap/version.py').read())
+
+setup(
+    name='pyvap',
+    version=__version__,
+    description='Kinetic model of particle evaporation',
+    long_description=long_description,
+    url='https://github.com/awbirdsall/pyvap',
+    author='Adam Birdsall',
+    author_email='abirdsall@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords=['kinetics', 'chemistry', 'evaporation'],
+    package_dir = {'': 'src'},
+    packages=['pyvap'],
+    install_requires=['matplotlib>=1.5','numpy','scipy'],
+    package_data={},
+    include_package_data=False,
+    entry_points={
+        # entry point for command line interface would go here
+        # 'console_scripts': [
+        #     'pyccutof=pyccutof.command_line:command',
+        # ],
+    },
+)
---
+++ src/pyvap/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''PYVAP: kinetic model of particle evaporation
+
+Core functionality in pyvap.main.
+'''
+from .version import __version__
+from .main import *
